06219b3029707aa8a8dd26bf8216b78260e596e8
DipMan <dipmanet2021@gmail.com>
2026-07-28 16:35:47 +0545

    made fixes for deployment

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from decouple import config
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', config(
-        'DJANGO_SETTINGS_MODULE', default='core.settings.dev'))
+        'DJANGO_SETTINGS_MODULE', default='core.settings.prod'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
